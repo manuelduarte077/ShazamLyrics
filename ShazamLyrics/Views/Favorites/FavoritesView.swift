ae8a65d41d28e79492579e876ebbc611b2196391
Manuel Duarte <carlosd99m@gmail.com>
2024-10-12 16:56:00 -0600

    Refactor FavoritesView to comment out unused code

--- ShazamLyrics/Views/Favorites/FavoritesView.swift
+++ ShazamLyrics/Views/Favorites/FavoritesView.swift
@@ -6,17 +6,17 @@ struct FavoritesView: View {
     
     var body: some View {
         ScrollView {
-            VStack(spacing: 10) {
-                ForEach(0 ..< 10) { item in
-                    CardView(item: item, isSelected: selectedItem == item)
-                        .onTapGesture {
-                            withAnimation(.easeInOut) {
-                                selectedItem = selectedItem == item ? nil : item
-                            }
-                        }
-                }
-            }
-            .navigationTitle("List of Lyrics")
+//            VStack(spacing: 10) {
+//                ForEach(0 ..< 10) { item in
+////                    CardView(item: item, isSelected: selectedItem == item)
+////                        .onTapGesture {
+////                            withAnimation(.easeInOut) {
+////                                selectedItem = selectedItem == item ? nil : item
+////                            }
+////                        }
+//                }
+//            }
+//            .navigationTitle("List of Lyrics")
         }
         
     }
